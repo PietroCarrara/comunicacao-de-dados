fbaaead88ca1f4586bba284a1f54b4b13e805818
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2024-01-19 11:16:12 -0300

    Start working on final project

---
+++ final/slice_qam.m
@@ -0,0 +1,18 @@
+function [out] = slice_qam(symbol)
+% Pega um número vetor de números complexos e retorna um vetor com os símbolos 64-QAM
+% mais próximos de cada entrada
+    QAM_OPTIONS = [-7 -5 -3 -1  1  3  5  7];
+    out = ones(size(symbol)) * complex(999, 999);
+    min_dist = ones(size(symbol)) * Inf;
+    for i = 1:length(QAM_OPTIONS)
+        for j = 1:length(QAM_OPTIONS)
+            x = QAM_OPTIONS(i);
+            y = QAM_OPTIONS(j);
+
+            dist = sqrt((real(symbol)-x).^2 + (imag(symbol)-y).^2);
+
+            out(dist <= min_dist) = complex(x, y);
+            min_dist(dist <= min_dist) = dist(dist <= min_dist);
+        end
+    end
+end
